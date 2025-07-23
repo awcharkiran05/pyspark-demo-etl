1fbabc159ed7ad3ba25efa644f65f5d3246ca94b
Kiran Awchar <awcharkiran05.com>
2025-07-23 13:40:52 +0000

    added india filter logic to customer_etl.py

--- etl_jobs/customer_etl.py
+++ etl_jobs/customer_etl.py
@@ -5,5 +5,5 @@ spark = SparkSession.builder.appName("CustomerETL").getOrCreate()
 data = [("John", "USA"), ("Amit", "India"), ("Sara", "India")]
 df = spark.createDataFrame(data, ["name", "country"])
 
-df_filtered = df.filter(df["country"] == "USA")
+df_filtered = df.filter(df["country"] == "INDIA")
 df_filtered.show()
